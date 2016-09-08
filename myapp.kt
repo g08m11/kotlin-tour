240b4d9d5ea830bb1827ac878f9b5648e5317d3c
Masaru Gushiken <gushiken@zigexn.co.jp>
2016-09-08 17:45:04 +0900

    update: update main logic.use to character

--- myapp.kt
+++ myapp.kt
@@ -1,4 +1,14 @@
-// made a kotlin first app
+/*
+ 変数
+   - val:型推論(Stringなどの用途目的の型を定義しないとエラーとなる)
+   - var:valよりも楽。書き方はSwiftやRubyっぽい
+*/
+
+
 fun main(args: Array<String>) {
-  println("HEllo World")
+  val msg: String = "Neo Hello World"
+  println(msg)
+
+  var msg2 = "Neo Hello World2"
+  println(msg2)
 }
