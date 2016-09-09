26e16ce2078c6364bc14fd2bbfeb1e0b3e968c04
Masaru Gushiken <gushiken@zigexn.co.jp>
2016-09-09 14:11:39 +0900

    update: check calculate

--- calculate.kt
+++ calculate.kt
@@ -1,6 +1,8 @@
 /*
   四則演算は他言語同様 + - * / % で対応可能
-
+  インクリメント、デクリメントも対応可能
+  代入演算子も対応可能
+  論理演算子も対応可能(varではなくvalなのが一般的?)
 */
 
 fun main(args: Array<String>) {
@@ -8,4 +10,17 @@ fun main(args: Array<String>) {
   println(x / 3 )
   println(x / 3.0 )
   println(x % 3.0 )
+
+  var y = 10
+  y++
+  println(y)
+  y--
+  println(y)
+
+  var z = 12
+  z += 6
+  println(z)
+
+  val flag = true
+  println(!flag)
 }
